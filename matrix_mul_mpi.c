177fd9595a82a271d6988dcd399f141bac885da3
Vivek Bangera <bangera.vivek@gmail.com>
2014-03-24 18:08:38 +0530

    Mpi mat mul actually does not contain MPI code. Test commit and push to Github

---
+++ matrix_mul_mpi.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+//#include<unistd.h>
+#define N 4
+
+int** Make2DIntArray(int arraySizeX, int arraySizeY) {
+int** theArray;
+theArray = (int**) malloc(arraySizeX*sizeof(int*));
+int i;
+for (i = 0; i < arraySizeX; i++)
+   theArray[i] = (int*) malloc(arraySizeY*sizeof(int));
+int j;
+
+for (i=0;i<arraySizeX;i++)
+{
+    for (j=0;j<arraySizeY;j++)
+    {
+        theArray[i][j]=rand()%100;
+    }
+}
+
+   return theArray;
+}
+
+void init_zeros(int** matrix)
+{
+	int i,j;
+	for (i=0;i<N;i++)
+	{	
+		for (j=0;j<N;j++)
+		{
+			matrix[i][j]=0;
+		}
+	}
+}
+
+void printmat(int** matrix)
+{
+	int i,j;
+	
+	for (i=0;i<N;i++)
+	{	
+		printf("\n");
+		for (j=0;j<N;j++)
+		{
+			printf("%d \t",matrix[i][j]);
+		}
+	}
+	printf("\n");
+}
+
+void main()
+{
+	int** M1=Make2DIntArray(N,N);
+	int** M2=Make2DIntArray(N,N);
+	int** Prod=Make2DIntArray(N,N);
+	
+	printmat(M1);
+	printmat(M2);
+	init_zeros(Prod);
+	int i,j,k;
+	
+	struct timeval start, end;
+	gettimeofday(&start, NULL);
+	
+	#pragma omp parallel for private(i,j,k)
+	for (i=0;i<N;i++)
+	{
+		//printf("Thread rank: %d\n", omp_get_thread_num());
+		
+		for (j=0;j<N;j++)
+		{
+
+			for (k=0;k<N;k++)
+			{
+				Prod[i][j]+=M1[i][k]*M2[k][j];
+			}
+		}
+	}
+
+	gettimeofday(&end, NULL);
+
+	double delta = ((end.tv_sec  - start.tv_sec) * 1000000u + 
+	         end.tv_usec - start.tv_usec) / 1.e6;
+
+	printmat(Prod);
+	printf("\nTime spent=%f \n", delta);
+	
+	
+}
+
