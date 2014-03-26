42e58bfb214948fc3c33d55f9eba81a9ed3ab288
Vivek Bangera <bangera.vivek@gmail.com>
2014-03-26 14:37:02 +0530

    Some issues fixed. MPI multiply still not working

--- matrix_mul_mpi.c
+++ matrix_mul_mpi.c
@@ -95,6 +95,7 @@ void main(int argc, char *argv[])
 		extra=N%slaves;
 		offset=0;
 		master_rows=stripsize;
+
 		for (dest=1;dest<=slaves;dest++)
 		{
 			rows = (dest <= extra) ? stripsize+1 : stripsize;   	
@@ -104,9 +105,10 @@ void main(int argc, char *argv[])
 			printf("\n Offset sent");
 			MPI_Send(&rows, 1, MPI_INT, dest, FROM_MASTER, MPI_COMM_WORLD);
 			printf("\n Stripsize sent");
-			MPI_Send(&M1[offset][0], rows*N, MPI_DOUBLE, dest , FROM_MASTER, MPI_COMM_WORLD);
+			printf("\n %p \n", &M1[offset][0]);
+			MPI_Send(M1[offset], rows*N, MPI_DOUBLE, dest , FROM_MASTER, MPI_COMM_WORLD);
 			printf("\n Matrix strip sent");
-			//MPI_Send(&M2, N*N, MPI_DOUBLE, i, FROM_MASTER, MPI_COMM_WORLD);
+			MPI_Send(M2[0], N*N, MPI_DOUBLE, dest, FROM_MASTER, MPI_COMM_WORLD);
 			printf("\n Matrix 2  sent");
 			offset+=rows;
 			printf("\n Offset incremented\n");
@@ -129,7 +131,7 @@ void main(int argc, char *argv[])
 		{
 			MPI_Recv(&offset, 1, MPI_INT, i, FROM_SLAVE, MPI_COMM_WORLD, &status);
 			MPI_Recv(&rows, 1, MPI_INT, i, FROM_SLAVE, MPI_COMM_WORLD, &status);
-			MPI_Recv(&Prod[offset][0], rows*N, MPI_DOUBLE, i, FROM_SLAVE, MPI_COMM_WORLD, &status);
+			MPI_Recv(Prod[offset], rows*N, MPI_DOUBLE, i, FROM_SLAVE, MPI_COMM_WORLD, &status);
 		}
 		printf("\n Received Rows");
 		//printmat(Prod);
@@ -143,9 +145,10 @@ void main(int argc, char *argv[])
 		printf("\n Rank %d received offset", rank);
 		MPI_Recv(&rows, 1, MPI_INT, 0, FROM_MASTER, MPI_COMM_WORLD, &status);
 		printf("\n Rank %d received rows", rank);
-		MPI_Recv(&M1, rows*N, MPI_DOUBLE, 0 , FROM_MASTER, MPI_COMM_WORLD, &status);
+		//printf("\n Slave %d has M1 as %p \n", rank, &M1);
+		MPI_Recv(M1[0], rows*N, MPI_DOUBLE, 0 , FROM_MASTER, MPI_COMM_WORLD, &status);
 		printf("\n Rank %d received M1", rank);
-		//MPI_Recv(&M2, N*N, MPI_DOUBLE, 0, FROM_MASTER, MPI_COMM_WORLD, &status);
+		MPI_Recv(&M2[0], N*N, MPI_DOUBLE, 0, FROM_MASTER, MPI_COMM_WORLD, &status);
 		printf("\n Rank %d starting matrix multiply", rank);
 		loopsize=offset+rows;
 		for (i=0;i<rows;i++)
@@ -163,7 +166,7 @@ void main(int argc, char *argv[])
 		}
 		MPI_Send(&offset, 1, MPI_INT, 0, FROM_SLAVE, MPI_COMM_WORLD);
 		MPI_Send(&rows, 1, MPI_INT, 0, FROM_SLAVE, MPI_COMM_WORLD);
-		MPI_Send(&Prod, rows*N, MPI_DOUBLE, 0, FROM_SLAVE, MPI_COMM_WORLD);
+		MPI_Send(Prod[0], rows*N, MPI_DOUBLE, 0, FROM_SLAVE, MPI_COMM_WORLD);
 	}
 	MPI_Finalize();
 	gettimeofday(&end, NULL);
